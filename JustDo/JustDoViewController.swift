10833943d9d8806920b48a4d5ff978d5f8e2fa5f
Jumageldi <jumamammedow01@gmail.com>
2024-06-06 17:56:53 +0300

    searchBar added

--- JustDo/JustDoViewController.swift
+++ JustDo/JustDoViewController.swift
@@ -6,14 +6,100 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import CoreData
+class JustDoViewController: UITableViewController {
+    var itemArray = [Items]()
+    let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadItems()
     }
 
+    // MARK: - TableView Data Sources Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        return cell
+    }
+    // MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        context?.delete(itemArray[indexPath.row])
+        itemArray.remove(at: indexPath.row)
+        
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    @IBAction func addButtonClicked(_ sender: UIBarButtonItem) {
+        var textfield = UITextField()
+        
+        let alert = UIAlertController(title: "Yeni öğe ekle", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ekle", style: .default) { action in
 
+            let newItem = Items(context: self.context!)
+            newItem.title = textfield.text!
+            newItem.done = false
+            self.itemArray.append(newItem)
+            self.saveItems()
+            
+        }
+        alert.addTextField { alertTextField in
+            alertTextField.placeholder = "Add New Item"
+            textfield = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    //MARK: - Model MAnipulations Model
+
+    func saveItems(){
+
+        do{
+            try context?.save()
+        }
+        catch{
+            print("Error saving the context \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    func loadItems(with request: NSFetchRequest<Items> = Items.fetchRequest(), predicate: NSPredicate? = nil){
+        do{
+            itemArray = try context!.fetch(request)
+        }catch{
+            print("Error fetching data from context \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    
+    }
+
+extension JustDoViewController: UISearchBarDelegate{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Items> = Items.fetchRequest()
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        loadItems(with: request, predicate: predicate)
+        
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0{
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+        }
+    }
 }
+
+
 
